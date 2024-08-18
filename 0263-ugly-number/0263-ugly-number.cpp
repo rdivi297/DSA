aeaf6150c36a319f4fd86c2ae77eb9e8b9ab90ea
Rahul Divi <98726386+rdivi297@users.noreply.github.com>
2024-08-18 08:22:54 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (9.61%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+         if(n==0) return false;
+        for(int prime:{2,3,5}){
+            while(n%prime==0) n/=prime;
+        }
+        return n==1;
+    } 
+};
