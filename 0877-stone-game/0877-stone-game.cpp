44a7f3abc85c9f765718272e49a6fd5ff0bda7aa
Rahul Divi <98726386+rdivi297@users.noreply.github.com>
2024-08-20 10:25:56 +0530

    Time: 10 ms (33.87%), Space: 10.7 MB (39.14%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
-
+    int dp[501][501];
+    int help(int l,int r,vector<int>&p){
+        if(l>r) return 0;
+        if(dp[l][r]!=-1) return dp[l][r];
+       int left=(r-l)%2==0?p[l]:0;
+       int right=(r-l)%2==0?p[r]:0;
+       dp[l][r]=max(help(l+1,r,p)+left,help(l,r-1,p)+right);
+    return dp[l][r];
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(dp,-1,sizeof(dp));
+        int sum=accumulate(piles.begin(),piles.end(),0);
+        return help(0,piles.size()-1,piles)>sum/2;
     }
 };
