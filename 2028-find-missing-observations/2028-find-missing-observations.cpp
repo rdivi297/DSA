81f0f1996c812295c56cb7bf8e9c0661dc4876aa
Rahul Divi <98726386+rdivi297@users.noreply.github.com>
2024-09-05 08:19:04 +0530

    Time: 97 ms (61.27%), Space: 116.4 MB (69.12%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -5,7 +5,7 @@ public:
          
         int sum1=accumulate(rolls.begin(),rolls.end(),0);
         int sum=mean*(n+m)-sum1;
-        if(sum<n || n*6<sum) return {};
+        if( sum<n ||n*6<sum) return {};
         vector<int>ans(n,sum/n);
         for(int i=0;i<sum%n;i++){
             ans[i]++;
