bf1121d78991b1d34aa262961b8f442a4b832f42
Rahul Divi <98726386+rdivi297@users.noreply.github.com>
2024-09-07 22:29:37 +0530

    Time: 726 ms (64.13%), Space: 0B (100.00%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+select a.employee_id , a.name , count(b.reports_to) as reports_count 
+, round(avg(b.age)) as average_age from employees a join employees b on a.employee_id=b.reports_to
+group by a.employee_id, a.name order by a.employee_id
