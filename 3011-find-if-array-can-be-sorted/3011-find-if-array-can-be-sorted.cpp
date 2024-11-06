d99cd5f62b1edff9f6d43a49c3531cac7af84f52
Rahul Divi <98726386+rdivi297@users.noreply.github.com>
2024-11-06 11:26:10 +0530

    Time: 1 ms (56.83%), Space: 31.1 MB (94.95%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        int n = nums.size();
-
-        // Avoid modifying the input directly
-        // Create a copy of the input array
-        
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (nums[j] <= nums[j + 1]) {
-                    // No swap needed
-                    continue;
-                } else {
-                    if (__builtin_popcount(nums[j]) ==
-                        __builtin_popcount(nums[j + 1])) {
-                        // Swap the elements
-                        swap(nums[j], nums[j + 1]);
-                    } else {
-                        return false;
-                    }
-                }
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]<=nums[j+1]) continue;
+                else if(__builtin_popcount(nums[j])==__builtin_popcount(nums[j+1])) swap(nums[j],nums[j+1]) ;
+                else return false;
             }
-        }
-        return true;
+        } return true;
     }
 };
