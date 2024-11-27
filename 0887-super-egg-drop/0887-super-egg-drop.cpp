d67f154dbcb90a823463cfa673bdbd0f0d47fa0f
Rahul Divi <98726386+rdivi297@users.noreply.github.com>
2024-11-28 00:12:59 +0530

    Time: 47 ms (70.09%), Space: 32.2 MB (24.95%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    
-    
-    int solve(int k, int n,vector<vector<int>>&dp){
-        if(n==0 || n==1) return n;
+   
+    int solve(int k,int n,vector<vector<int>>&dp) {
         if(k==1) return n;
+        if(n==0 || n==1) return n;
         if(dp[k][n]!=-1) return dp[k][n];
+        int l=0;
+        int h=n;
         int mn=INT_MAX;
-        int l=1;int h=n;
-        while(l<=h){
+        while(l<=h) {
             int m=(l+h)/2;
-            int b=solve(k-1,m-1,dp);
-            int nb=solve(k,n-m,dp);
-            if(b>nb) h=m-1;
-            else l=m+1;
-            int temp=1+max(b,nb);
+            int eggbreak = dp[k-1][m-1]==-1?solve(k-1,m-1,dp):dp[k-1][m-1];
+            int noeggbreak = dp[k][n-m]==-1?solve(k,n-m,dp):dp[k][n-m];
+            int temp=1+max(eggbreak,noeggbreak);
             mn=min(mn,temp);
-        }
+            if(eggbreak>noeggbreak) h=m-1;
+            else l=m+1;
+        } 
         return dp[k][n]=mn;
     }
+    
     int superEggDrop(int k, int n) {
         vector<vector<int>>dp(k+1,vector<int>(n+1,-1));
-        
-        return solve(k,n,dp);
+       int ans=solve(k,n,dp);
+     return ans;
     }
 };
